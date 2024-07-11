c573872ffbdce02cc0e6698f67eaafd8e13957b7
NhatNguyen2805 <nhatnguyen282005>
2024-07-11 20:15:57 +0700

    Update Func-Multi

--- Program.cs
+++ Program.cs
@@ -1,28 +1,20 @@
-﻿using System;
-
-namespace Week1
+﻿namespace Mutli
 {
-    class Program
+    internal class Program
     {
-        public static int Sum(int a, int b)
+        private static void Main(string[] args)
         {
-            return a + b;
-        }
-        public static double Div(int a, int b)
-        {
-            return a / b;
-        }
-        static void Main(string[] args)
-        {
-            int a, b;
-            Console.Write("Nhap a, b: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Tong 2 so: "+Sum(a, b));
-            Console.WriteLine("Thuong 2 so: "+Div(a, b));
-            Console.ReadKey();
-        }
+            int number_One, number_Two;
+            int ans_MultiDigits;
 
+            Console.WriteLine("Enter value Number One:");
+            number_One = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value Number Two:");
+            number_Two = int.Parse(Console.ReadLine());
+
+            ans_MultiDigits = number_One * number_Two;
+
+            Console.WriteLine("Multi number {0} and number {1} :  {2}", number_One, number_Two, ans_MultiDigits);
+        }
     }
-
 }
