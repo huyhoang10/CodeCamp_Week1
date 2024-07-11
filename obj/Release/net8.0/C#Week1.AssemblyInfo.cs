6bbd5a3b7dc9a8e6cf1815ab2ba6c3779c2baf2c
NhatNguyen2805 <nhatnguyen282005>
2024-07-11 21:06:48 +0700

    Update Func-Multi

--- obj/Release/net8.0/C#Week1.AssemblyInfo.cs
+++ obj/Release/net8.0/C#Week1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Week1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a084661b5b77e42d721da0b2e70689ab6f01f38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Week1")]
